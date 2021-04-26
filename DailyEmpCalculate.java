f865459adab606bac481e1be788da9404bfeb7dc
Renuka Sawale <rrsuryawanshi11@gmail.com>
2021-04-26 22:53:44 +0530

    Added daily employee wage

---
+++ DailyEmpCalculate.java
@@ -0,0 +1,19 @@
+public class DailyEmpCalculate {
+        public static void main(String[] args) {
+                //Constants
+                int IS_FULL_TIME = 1;
+                int EMP_RATE_PER_HR = 20;
+                double empCheck=Math.floor(Math.random() * 10) % 2;
+                int empHrs;
+                if(empCheck == IS_FULL_TIME) {
+                        empHrs=8;
+    	        }
+                else {
+                        empHrs=0;
+                }
+                int salary = EMP_RATE_PER_HR*empHrs;
+                System.out.println("Daily Employee Wage Calculation is:" + salary);
+        }
+
+}
+
